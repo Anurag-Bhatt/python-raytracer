91044710c0fa24ad79f96086f387807270d5addf
Anurag_Bhatt <anuragbhatt865@gmail.com>
2025-12-23 15:20:18 +0530

    Made some bug fixes

--- src/interval.py
+++ src/interval.py
@@ -1,22 +1,32 @@
 from math import inf
 
+
 class Interval:
     empty: "Interval"
     universe: "Interval"
 
     def __init__(self, min=inf, max=-inf) -> None:
         
-        self.min = min
-        self.max = max
+        self.min:float = min
+        self.max:float = max
 
-    def size(self):
+    def size(self) -> float:
         return self.max - self.min
     
-    def contains(self, x):
+    def contains(self, x)-> bool:
         return self.min <= x and x <= self.max
     
-    def surrounds(self, x):
+    def surrounds(self, x) -> bool:
         return self.min < x and x < self.max
+    
+    def clamp(self, x:float) -> float:
+        if x < self.min:
+            return self.min
+        if x > self.max:
+            return self.max
+        
+        return x
 
 Interval.empty = Interval(inf, -inf)
 Interval.universe = Interval(-inf, inf)
+
