97a08942d186915eea8d4f895052ddf19b3fcaef
Anurag_Bhatt <anuragbhatt865@gmail.com>
2025-12-30 20:36:18 +0530

    Bug fixes: Vector multiplication

--- src/vec3.py
+++ src/vec3.py
@@ -1,4 +1,4 @@
-import math
+import math 
 from utility import random_double, random_range
 
 class Vec3:
@@ -15,7 +15,10 @@ class Vec3:
         return Vec3(self.x - other.x, self.y - other.y, self.z - other.z)
 
     def __mul__(self, other):
-        return Vec3(self.x * other, self.y * other, self.z * other)
+        if isinstance(other, Vec3):
+            return Vec3(self.x * other.x, self.y * other.y, self.z * other.z)
+        else:
+            return Vec3(self.x * other, self.y * other, self.z * other)
 
     def __rmul__(self, other):
         return self * other
@@ -35,6 +38,11 @@ class Vec3:
     def length(self):
         return math.sqrt(self.length_squared())
     
+    def near_zero(self):
+        # Returns true if the vector is close to zero in all three dimensions
+        s = 1e-8
+        return abs(self.x) < s and abs(self.y) < s and abs(self.z) < s
+
     @staticmethod
     def random_vec3():
         return Vec3(random_double(), random_double(), random_double())
@@ -73,3 +81,7 @@ def random_on_hemisphere(normal:Vec3):
         return on_unit_sphere
     else:
         return -on_unit_sphere
+    
+# Reflects the vector V using vector maths
+def reflect(v:Vec3, n:Vec3):
+    return v - 2*dot(v,n)*n
