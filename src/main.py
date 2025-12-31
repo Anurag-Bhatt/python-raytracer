a9fbf346c52050ca8572b49ec9ce8a23707ea23b
Anurag_Bhatt <anuragbhatt865@gmail.com>
2025-12-31 16:46:20 +0530

    Scene Tree update

--- src/main.py
+++ src/main.py
@@ -16,27 +16,32 @@ def main():
     
     world = HittableList()
 
-    R = cos(pi/4)
+    #R = cos(pi/4)
 
-    material_left = material.Lambertian(color(0, 0, 1))
-    material_right = material.Lambertian(color(1, 0, 0))
+    #material_left = material.Lambertian(color(0, 0, 1))
+    #material_right = material.Lambertian(color(1, 0, 0))
 
-    #material_ground             = material.Lambertian(color(0.8, 0.8, 0.0))
-    #material_center             = material.Lambertian(color(0.1, 0.2, 0.5))
-    #material_left               = material.Dielectric(1.50)
-    #material_inner_bubble       = material.Dielectric(1.0 / 1.50)
-    #material_right              = material.Metal(color(0.8, 0.6, 0.2), 1.0)
-
-    #world.add(Sphere(Point3(0, -100.5, -1), 100, material_ground))
-    #world.add(Sphere(Point3(0, 0, -1.2), 0.5, material_center))
-    #world.add(Sphere(Point3(-1, 0, -1), 0.5, material_left))
-    #world.add(Sphere(Point3(-1, 0, -1), 0.4, material_inner_bubble))
-    #world.add(Sphere(Point3(1, 0, -1), 0.5, material_right))
+    material_ground             = material.Lambertian(color(0.8, 0.8, 0.0))
+    material_center             = material.Lambertian(color(0.1, 0.2, 0.5))
+    material_left               = material.Dielectric(1.50)
+    material_inner_bubble       = material.Dielectric(1.0 / 1.50)
+    material_right              = material.Metal(color(0.8, 0.6, 0.2), 1.0)
     
-    world.add(Sphere(Point3(-R, 0, -1), R, material_left))
-    world.add(Sphere(Point3(R, 0, -1), R, material_right))
+    world.add(Sphere(Point3(0, -100.5, -1), 100, material_ground))
+    world.add(Sphere(Point3(0, 0, -1.2), 0.5, material_center))
+    world.add(Sphere(Point3(-1, 0, -1), 0.5, material_left))
+    world.add(Sphere(Point3(-1, 0, -1), 0.4, material_inner_bubble))
+    world.add(Sphere(Point3(1, 0, -1), 0.5, material_right))
+    
+    #world.add(Sphere(Point3(-R, 0, -1), R, material_left))
+    #world.add(Sphere(Point3(R, 0, -1), R, material_right))
 
     cam = Camera(width=400, aspect_ratio=16.0/9.0, sample_per_pixel=10)
+
+    cam.lookfrom = Point3(-2, 2, 1)
+    cam.lookat = Point3(0, 0, -1)
+    cam.view_up = Vec3(0, 1, 0)
+
     cam.initialize()
 
     im = Image.new("RGB", (cam.image_width, cam.image_height))
@@ -46,6 +51,6 @@ def main():
     
     im.show()
 
-    im.save("images/wide_fov_1.png")
+    im.save("images/wide_fov_3.png")
 
 main()
