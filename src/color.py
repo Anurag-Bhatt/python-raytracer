b049b92e17b3e66ec5c4a607f64fac6d766d4a8c
Anurag_Bhatt <anuragbhatt865@gmail.com>
2026-01-05 21:01:27 +0530

    removed old files

--- src/color.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def write_color(pixel_color: np.ndarray) -> tuple[int, int, int]:
-    
-    gamma_corrected = np.sqrt(np.clip(pixel_color, 0.0, 0.999))
-
-    rbyte, gbyte, bbyte = (gamma_corrected * 256).astype(int)
-    return rbyte, gbyte, bbyte
