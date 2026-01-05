e95a258db124cdf7c06119bae2edb7f3cdae753e
Anurag_Bhatt <anuragbhatt865@gmail.com>
2026-01-05 20:44:43 +0530

    (minor) removed unused functions

--- src/utility.py
+++ src/utility.py
@@ -18,19 +18,6 @@ def random_unit_vector()->np.ndarray:
         if 1e-8 < length_sqr <= 1:
             return p/(np.sqrt(length_sqr))
 
-# Reflects the vector V using vector maths
-def reflect(v:np.ndarray, n:np.ndarray):
-    return v - 2*np.dot(v,n)*n
-
-# Refraction for dielectric materials
-def refract(v:np.ndarray, n:np.ndarray, etai_over_etat):
-    
-    cos_theta = min(np.dot(-v, n), 1)
-    r_out_perp = etai_over_etat * (v + cos_theta * n)
-    r_out_para = -np.sqrt(max(0.0, 1.0 - np.dot(r_out_perp, r_out_perp))) * n
-
-    return r_out_perp + r_out_para
-
 # Helper functions
 def normalise(v:np.ndarray) -> np.ndarray:
     norm = np.linalg.norm(v)
